4a13a8cbe7299941a84840b7aa8137e221dae339
bq_bzh <Quentin@130.66.208.19>
2011-09-28 09:53:24 +0200

    1er commit qui sert à rien

---
+++ Test1/src/Classe1.java
@@ -0,0 +1,13 @@
+
+public class Classe1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
