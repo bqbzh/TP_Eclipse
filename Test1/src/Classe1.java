d2add6dbad6a4ee2c573cb55fe60de192e7b3192
bq_bzh <Quentin@130.66.208.19>
2011-09-28 09:59:11 +0200

    Ajout d'un deuxieme println

--- Test1/src/Classe1.java
+++ Test1/src/Classe1.java
@@ -7,7 +7,9 @@ public class Classe1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		System.out.println("I'm now the GitMaster");
 
 	}
+	
 
 }
